d81e49321c97d530a4ca5ac96d6003316c17c377
nevertheless.space <nevertheless.space@gmail.com>
2023-02-24 01:55:20 +0100

    minor improvements

--- build.ps1
+++ build.ps1
@@ -1,5 +1,13 @@
+$version = "X.X.X"
+
+Remove-Item -LiteralPath ".\dist" -Force -Recurse
+
 pip3 install -r requirements.txt
+
+(Get-Content -path .\index.py -Raw) -replace '{{version}}',"$version" | Set-Content -Path .\index.py
 
 python3 -m PyInstaller index.py --name ntl-flux-console --onefile --icon=imgs/ntl.ico -w
 
 Copy-Item 'imgs' 'dist' -Recurse -Force
+
+Compress-Archive -Path ".\dist\*" -DestinationPath ".\dist\ntl-flux-console-windows-$version.zip"
